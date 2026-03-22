07002b56f6c29979313285d898bdb5e7f160d1f0
skye <swskye17@gmail.com>
2026-03-22 23:58:07 +0300

    fix storage module issue, downgrade to stable version

--- mobile/Vellichor/android/app/src/main/java/com/vellichor/MainActivity.kt
+++ mobile/Vellichor/android/app/src/main/java/com/vellichor/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vellichor
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -14,9 +15,16 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "Vellichor"
 
   /**
+   * IMPORTANT: Required for react-native-gesture-handler
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)  // Pass null instead of savedInstanceState
+  }
+
+  /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
--- mobile/Vellichor/android/app/src/main/java/com/vellichor/MainApplication.kt
+++ mobile/Vellichor/android/app/src/main/java/com/vellichor/MainApplication.kt
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.learnium.RNDeviceInfo.RNDeviceInfo
 
 class MainApplication : Application(), ReactApplication {
 
@@ -13,10 +14,12 @@ class MainApplication : Application(), ReactApplication {
     getDefaultReactHost(
       context = applicationContext,
       packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
+      PackageList(this).packages.apply {
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // add(MyReactNativePackage())
+        add(RNDeviceInfo())
+
+      },
     )
   }
 
